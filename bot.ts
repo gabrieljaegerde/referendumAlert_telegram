10a6977a50f43fbeb590950d99332b19e3b7308c
Gabriel <gabrieljaeger1@gmail.com>
2022-02-14 18:45:42 +0300

    only admin can execute command

--- bot.ts
+++ bot.ts
@@ -19,11 +19,11 @@ export const start = async (): Promise<{ runnerHandle: RunnerHandle, tBot: Bot; 
    */
 
   bot.command("newReferendumBroadcastOn", async (ctx) => {
-    if (checkIsGroup(ctx)) {
+    if (checkIsGroup(ctx, true)) {
       const group = await getGroupOrCreate(ctx);
       const userCol = await getUserCollection();
       if (group.broadcast && !group.blocked) {
-        const message = `Referendum alerts already on\\.`;
+        const message = `Referendum alerts turned on\\.`;
         await send(group.chatId, message, "MarkdownV2");
       }
       else{
@@ -36,7 +36,7 @@ export const start = async (): Promise<{ runnerHandle: RunnerHandle, tBot: Bot; 
   })
 
   bot.command("newReferendumBroadcastOff", async (ctx) => {
-    if (checkIsGroup(ctx)) {
+    if (checkIsGroup(ctx, true)) {
       const group = await getGroupOrCreate(ctx);
       const userCol = await getUserCollection();
       if (group.broadcast) {
